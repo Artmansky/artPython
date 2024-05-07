72de3c75a2e38f8b6f60cca3dd704d7bb7953fa5
Artmansky <tomaszartmanski@gmail.com>
2024-05-07 19:45:03 +0200

    Torch and filepaths added

---
+++ artProject.py
@@ -0,0 +1,10 @@
+import torch as TO
+import torchvision as TV
+import torchvision.transforms as TT
+
+BAROQUE_PATH = "./baroque"
+CUBISM_PATH = "./cubism"
+POPART_PATH = "./pop-art"
+POSTIMP_PATH = "./Postimpressionism"
+
+#TODO: Custom Loader
